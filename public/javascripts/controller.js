9ade103be2f2423ea9ab7558827397b2e32f3bda
briandmpls <briandmpls@gmail.com>
2015-10-30 15:11:41 -0500

    test

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -40,15 +40,15 @@ app.controller('MainController', ['$scope','$http', function($scope,$http) {
                 getLocation();
 
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        });
-    }
-
-
-    initMap();
+    //function initMap() {
+    //    map = new google.maps.Map(document.getElementById('map'), {
+    //        center: {lat: -34.397, lng: 150.644},
+    //        zoom: 8
+    //    });
+    //}
+    //
+    //
+    //initMap();
 
 }]);
 
